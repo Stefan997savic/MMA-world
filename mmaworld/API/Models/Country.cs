f83511ddb13d9b9741746dcd950a4dafe2bc08ed
Luka Radovanovic <luka.radovanovic@teletrader.com>
2024-02-27 20:08:58 +0100

    Implementation - Added models directory and Country class explanation

---
+++ mmaworld/API/Models/Country.cs
@@ -0,0 +1,10 @@
+﻿namespace API.Models
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ISO2 { get; set; }
+        public string ISO3 { get; set; }
+    }
+}
