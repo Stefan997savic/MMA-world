4cfd0fc05839636c52932f63e8eb07dfd8e3f3e1
Stefan997savic <stefan997savic@gmail.com>
2024-02-28 19:55:04 +0100

    update -models/country

--- mmaworld/API/Models/Country.cs
+++ mmaworld/API/Models/Country.cs
@@ -7,5 +7,16 @@
         public string ISO2 { get; set; }
         public string ISO3 { get; set; }
         public string NameSRB { get; set; }
+
+
+
+        public Country(int id, string name, string iSO2, string iSO3, string nameSRB)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.ISO2 = iSO2;
+            this.ISO3 = iSO3;
+            this.NameSRB = nameSRB;
+        }
     }
 }
