910a432881f29c62dfbb3ad535048e9ea8cdc55e
Stefan997savic <stefan997savic@gmail.com>
2024-03-05 17:13:23 +0100

    create Models/Fighter.cs

---
+++ mmaworld/API/Models/Fighter.cs
@@ -0,0 +1,17 @@
+﻿namespace API.Models
+{
+    public class Fighter
+    {
+        public string NickName { get; set; }
+
+        public string Record {  get; set; }
+
+
+        public Fighter(string nick,string record) 
+        {
+            NickName = nick;
+            Record = record;
+        }
+
+    }
+}
