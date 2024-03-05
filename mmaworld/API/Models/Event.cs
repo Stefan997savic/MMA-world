d166197871dc155dcf8d874cfe62ee561acc1237
Stefan997savic <stefan997savic@gmail.com>
2024-03-05 17:52:41 +0100

    implement Models/Event.cs

---
+++ mmaworld/API/Models/Event.cs
@@ -0,0 +1,18 @@
+﻿namespace API.Models
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Host Host { get; set; }
+        public List<Match> Matches { get; set; }
+
+        public Event(int id, string name, Host host, List<Match> matches)
+        {
+            Id = id;
+            Name = name;
+            Host = host;
+            Matches = new List<Match>();
+        }
+    }
+}
--- mmaworld/API/Models/Match.cs
+++ mmaworld/API/Models/Match.cs
@@ -2,5 +2,28 @@
 {
     public class Match
     {
+        public int Id { get; set; }
+        public Fighter BlueFigter { get; set; }
+
+        public Fighter RedFighter { get; set; }
+
+        public Referee Referee { get; set; }
+
+        public Fighter Winner {  get; set; }
+
+        public ScoreType ScoreType { get; set; }
+
+        public TimeSpan TimeElapsed { get; set; }
+
+        public Match(int id, Fighter blueFigter, Fighter redFighter, Referee referee, Fighter winner, ScoreType scoreType, TimeSpan timeElapsed)
+        {
+            Id = id;
+            BlueFigter = blueFigter;
+            RedFighter = redFighter;
+            Referee = referee;
+            Winner = winner;
+            ScoreType = scoreType;
+            TimeElapsed = timeElapsed;
+        }
     }
 }
