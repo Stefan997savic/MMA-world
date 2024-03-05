14bd3882bb4fc511f7d6e3cf6d6af445db5fe95c
Stefan997savic <stefan997savic@gmail.com>
2024-03-05 17:18:55 +0100

    implement Models/ScoreType.cs

---
+++ mmaworld/API/Models/ScoreType.cs
@@ -0,0 +1,16 @@
+﻿namespace API.Models
+{
+    public class ScoreType
+    {
+
+        public int Id {  get; set; }
+
+        public Fighter Name { get; set; }
+
+        public ScoreType(int id, Fighter name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
